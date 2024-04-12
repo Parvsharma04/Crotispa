60657fb2f1774067078fea86184d4f9c695ed9d0
Parv Sharma <sharmaparv.2004@gmail.com>
2024-04-12 22:33:30 +0530

    mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -163,67 +163,67 @@
 // });
 
 
-const express = require("express");
-const path = require("path");
-const app = express();
-const LogInCollection = require("./mongodb");
-const port = process.env.PORT || 3000;
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const LogInCollection = require("./mongodb");
+// const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
-const templatePath = path.join(__dirname, "../backend/templates");
-const publicPath = path.join(__dirname, "../backend/public");
-const homePath = path.join(__dirname, "../frontend/home.html");
+// const templatePath = path.join(__dirname, "../backend/templates");
+// const publicPath = path.join(__dirname, "../backend/public");
+// const homePath = path.join(__dirname, "../frontend/home.html");
 
-console.log(publicPath);
-app.set('view engine', 'hbs');
-app.set('views', templatePath);
-app.use(express.static(publicPath));
+// console.log(publicPath);
+// app.set('view engine', 'hbs');
+// app.set('views', templatePath);
+// app.use(express.static(publicPath));
 
-app.get('/signup', (req, res) => {
-  res.render('signup');
-});
+// app.get('/signup', (req, res) => {
+//   res.render('signup');
+// });
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
+// app.get('/login', (req, res) => {
+//   res.render('login');
+// });
 
-app.get('/home', (req, res) => {
-  res.sendFile(homePath);
-});
+// app.get('/home', (req, res) => {
+//   res.sendFile(homePath);
+// });
 
-app.post('/signup', async (req, res) => {
-  const data = {
-    name: req.body.name,
-    password: req.body.password
-  };
-  const checking = await LogInCollection.findOne({ name: req.body.name });
-  if (checking && checking.name === req.body.name && checking.password === req.body.password) {
-    res.send("user details already exist");
-  } else {
-    try {
-      await LogInCollection.insertMany([data]);
-      res.redirect('/home');
-    } catch (error) {
-      res.send("wrong inputs");
-    }
-  }
-});
+// app.post('/signup', async (req, res) => {
+//   const data = {
+//     name: req.body.name,
+//     password: req.body.password
+//   };
+//   const checking = await LogInCollection.findOne({ name: req.body.name });
+//   if (checking && checking.name === req.body.name && checking.password === req.body.password) {
+//     res.send("user details already exist");
+//   } else {
+//     try {
+//       await LogInCollection.insertMany([data]);
+//       res.redirect('/home');
+//     } catch (error) {
+//       res.send("wrong inputs");
+//     }
+//   }
+// });
 
-app.post('/login', async (req, res) => {
-  try {
-    const check = await LogInCollection.findOne({ name: req.body.name });
-    if (check && check.password === req.body.password) {
-      res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` });
-    } else {
-      res.send("incorrect password");
-    }
-  } catch (e) {
-    res.send("wrong details");
-  }
-});
+// app.post('/login', async (req, res) => {
+//   try {
+//     const check = await LogInCollection.findOne({ name: req.body.name });
+//     if (check && check.password === req.body.password) {
+//       res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` });
+//     } else {
+//       res.send("incorrect password");
+//     }
+//   } catch (e) {
+//     res.send("wrong details");
+//   }
+// });
 
-app.listen(port, () => {
-  console.log('port connected');
-});
+// app.listen(port, () => {
+//   console.log('port connected');
+// });
